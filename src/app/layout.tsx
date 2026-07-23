bb977ee2afa2398f75ec2fba0ca27e1e074c4773
Kymie <kyjstudies@gmail.com>
2026-07-23 16:07:17 -0300

    fix: fonte não encontrada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Nunito, Kosugi_Maru, Chiron_GoRound_TC } from "next/font/google";
+import { Nunito, Kosugi_Maru } from "next/font/google";
 import { HelloIntro } from "@/components/HelloIntro";
 import "../styles/globals.css";
 
